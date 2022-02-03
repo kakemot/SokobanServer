77428ef1f54a80b7b55490eed172fc23c83f016a
Tony Arntsen <tarntsen@enua.no>
2022-02-03 19:40:17 +0100

    Make sure db and table is created before use

--- SokobanServer/DbService.cs
+++ SokobanServer/DbService.cs
@@ -5,6 +5,7 @@
         public async void AddLevel(Level level)
         {
             await using var dbContext = new MyDbContext();
+            dbContext.Database.EnsureCreated();
             await dbContext.AddAsync(level);
             await dbContext.SaveChangesAsync();
         }
@@ -12,6 +13,7 @@
         public async Task<List<Level>> GetLevels()
         {
             await using var dbContext = new MyDbContext();
+            dbContext.Database.EnsureCreated();
             List<Level> level = dbContext.Levels.ToList();
             return level;
         }
@@ -19,6 +21,7 @@
         public async Task<Level> GetLevel(int id)
         {
             await using var dbContext = new MyDbContext();
+            dbContext.Database.EnsureCreated();
             Level level = dbContext.Levels.Where(x => x.Id == id).First();
             return level;
         }
